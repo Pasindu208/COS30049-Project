a9c631d7eca1997f0041ebb260731b64a1d15540
Pasindu Gamage <udnisap208@gmail.com>
2023-09-02 22:50:18 +1000

    ; (

--- src/Screens/home.js
+++ src/Screens/home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Components/navbar'
 import Homemid from '../Components/homemid'
+import Footer from '../Components/footer'
 
 const Home = () => {
   return (
     <div className="App">
       <Navbar />
       <Homemid />
+      <Footer />
     </div>
   )
 }

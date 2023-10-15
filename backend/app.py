5d09b1a73b7ab9bac9cf89c490a92f2d105281c1
Pasindu Gamage <udnisap208@gmail.com>
2023-10-15 20:57:31 +1100

    done

---
+++ backend/app.py
@@ -0,0 +1,142 @@
+from flask import Flask, request, jsonify
+import subprocess
+import os
+import mysql.connector
+import re
+from flask_cors import CORS
+import datetime
+
+decodedstring = ""
+
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'password',
+    'database': 'Audits',
+}
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    # Check if a file was posted
+    if 'file' not in request.files:
+        return jsonify({"error": "No file provided"}), 400
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({"error": "No file selected"}), 400
+
+    # Save the file temporarily
+    print(file.filename)
+    # file_path = os.path.join("/tmp", file.filename)
+    file_path = file.filename
+    file.save(file_path)
+
+    # Run Slither on the file and generate a markdown report
+    report_path = file_path + ".md"
+    print(report_path)
+    command = ["slither", file_path, "--checklist"]
+    process = subprocess.Popen(
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+
+    # if process.returncode != 0:
+    #     print(stderr.decode("utf-8"))
+    #     print(stdout.decode("utf-8"))
+    #     return jsonify({"error": stderr.decode("utf-8")}), 500
+
+    # with open(report_path, 'r') as f:
+    #     report = f.read()
+
+    # os.remove(file_path)  # Clean up the uploaded file
+    # os.remove(report_path)  # Clean up the generated report
+    print(stdout)
+
+    connection = mysql.connector.connect(
+        user='root', password='password', host='127.0.0.1', database='Audits', auth_plugin='mysql_native_password')
+
+    cursor = connection.cursor()
+
+    if connection.is_connected():
+        print('Connected to MySQL database')
+
+    else:
+        print("Not connected")
+
+    # return jsonify({"report": stdout.decode("utf-8")})
+    decodedstring = jsonify({"report": stdout.decode("utf-8")})
+
+    decodedstring = decodedstring.data.decode('utf-8')
+    # print("hi")
+    # print(decodedstring)
+
+    # Regular expression pattern
+    # pattern = r'## (.*?)\r\nImpact: (.*?)\r\nConfidence: (.*?)\r\n'
+    pattern = r'## (.*?)\\r\\nImpact: (.*?)\\r\\nConfidence: (.*?)\\r\\n'
+
+    # print(decodedstring)
+
+    # Find all matches
+    matches = re.findall(pattern, decodedstring)
+
+    # Extracted data
+    # for match in matches:
+    #     issue_title, severity, confidence = match
+    #     print(f"Issue Title: {issue_title}")
+    #     print(f"Severity: {severity}")
+    #     print(f"Confidence: {confidence}")
+    #     print("----")
+
+    for match in matches:
+        issue_title, severity, confidence = match
+        insert_query = "INSERT INTO Vulnerabilities (type, severity, confidence) VALUES (%s, %s, %s)"
+        cursor.execute(insert_query, (issue_title, severity, confidence))
+
+    current_date = datetime.datetime.now().date()
+
+    insert_query = "INSERT INTO SmartContracts (contract_name, upload_date) VALUES (%s, %s)"
+    cursor.execute(insert_query, (file_path, current_date))
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    # return decodedstring
+    return "ok"
+
+
+@app.route('/vulnerabilities', methods=['GET'])
+def get_vulnerabilities():
+    connection = mysql.connector.connect(
+        user='root', password='password', host='127.0.0.1', database='Audits', auth_plugin='mysql_native_password')
+
+    cursor = connection.cursor()
+
+    if connection.is_connected():
+        print('Connected to MySQL database')
+
+    else:
+        print("Not connected")
+
+    # get_query = "SELECT * FROM Vulnerabilities"
+    get_query = "SELECT V.type AS Vulnerability_Type, V.severity, V.confidence, R.recommendation FROM Vulnerabilities V INNER JOIN Recommendations R ON V.type = R.type"
+    # cursor.execute(get_query)
+    cursor.execute(get_query)
+    vulnerabilities = cursor.fetchall()
+
+    get_query2 = "SELECT contract_name, upload_date FROM SmartContracts"
+    cursor.execute(get_query2)
+    contracts = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+    data = [{'contracts': contracts, 'data': vulnerabilities}]
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- src/Components/submitContent.js
+++ src/Components/submitContent.js
@@ -1,42 +1,91 @@
 import React from "react";
 import "../Styles/submitContent.css";
 
-const SubmitContent = () => {
-    return (
-        <div>
-            <div className="contact-section">
-                <h2>Upload your Smart Contract</h2>
-                <p>
-                    Get your smart contract security audit report, and identify
-                    vulnerabilities in the contract code. Get recommendations on
-                    issues to address potential vulnerabilities in the smart
-                    contract.
-                </p>
-            </div>
+class SubmitContent extends React.Component {
+    uploadFile() {
+        console.log("hi");
+    
 
-            <div className="submit-section">
-                <input type="text" placeholder="First Name" />
-                <input type="text" placeholder="Last Name" />
-                <input type="email" placeholder="Email" />
-                <input type="tel" placeholder="Primary Contact Number" />
-                <input type="text" placeholder="Job Title" />
-                <input type="text" placeholder="Company Name" />
-                <input type="text" placeholder="Country" />
-                <input type="text" placeholder="Project Name" />
-                <input
-                    type="date"
-                    placeholder="Expected Project Completion Date"
-                />
-                <select>
-                    <option value="">Are you using Solidity?</option>
-                    <option value="yes">Yes</option>
-                    <option value="no">No</option>
-                </select>
-                <input type="file" accept=".sol" />
-                <button type="submit">Submit</button>
+    const fileInput = document.getElementById("fileInput");
+    const file = fileInput.files[0];
+
+    if (!file) {
+        alert("Please select a file.");
+        return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    fetch("http://127.0.0.1:5000/upload", {
+        method: "POST",
+        body: formData,
+    })
+        .then((response) => {
+            if (!response.ok) {
+                // throw new Error("Network response was not ok");
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log("File uploaded successfully:", data);
+            alert("File uploaded successfully!");
+        });
+    // .catch((error) => {
+    //     console.error(
+    //         "There was a problem with the fetch operation:",
+    //         error
+    //     );
+    //     alert("File upload failed. Please try again.");
+    // });
+
+    }
+    render() {
+        return (
+            <div>
+                <div className="contact-section">
+                    <h2>Upload your Smart Contract</h2>
+                    <p>
+                        Get your smart contract security audit report, and
+                        identify vulnerabilities in the contract code. Get
+                        recommendations on issues to address potential
+                        vulnerabilities in the smart contract.
+                    </p>
+                </div>
+
+                <div className="submit-section">
+                    <input type="text" placeholder="First Name" />
+                    <input type="text" placeholder="Last Name" />
+                    <input type="email" placeholder="Email" />
+                    <input type="tel" placeholder="Primary Contact Number" />
+                    <input type="text" placeholder="Job Title" />
+                    <input type="text" placeholder="Company Name" />
+                    <input type="text" placeholder="Country" />
+                    <input type="text" placeholder="Project Name" />
+                    <input
+                        type="date"
+                        placeholder="Expected Project Completion Date"
+                    />
+                    <select>
+                        <option value="">Are you using Solidity?</option>
+                        <option value="yes">Yes</option>
+                        <option value="no">No</option>
+                    </select>
+                    <label for="fileInput">Choose a file:</label>
+                    <input
+                        type="file"
+                        id="fileInput"
+                        name="fileInput"
+                        accept=".sol"
+                    />
+
+                    <button type="button" onClick={this.uploadFile}>
+                        Submit
+                    </button>
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default SubmitContent;
